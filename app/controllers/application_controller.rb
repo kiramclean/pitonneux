73960afb68f6df151b16460216c24d0b46dddef9
Kira McLean <kiraemclean@gmail.com>
2016-06-17 00:34:43 -0400

    complete events controller spec

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,6 @@ class ApplicationController < ActionController::Base
 
   def user_not_authorized
     flash[:alert] = t 'unauthorized'
-    redirect_to(request.referrer || root_path)
+    redirect_back(fallback_location: root_path)
   end
 end
